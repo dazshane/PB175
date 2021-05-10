e1632591411503e70dfbc5dabe0e470804a6274d
Marek Stosic <xstosic@fi.muni.cz>
2021-05-10 22:12:19 +0200

    db created

---
+++ test.js
@@ -0,0 +1,23 @@
+const sqlite3 = require('sqlite3').verbose();
+
+// open the database
+let db = new sqlite3.Database('./db/chinook.db', (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the chinook database.');
+});
+const sql = `
+    CREATE TABLE IF NOT EXISTS projects (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      password TEXT,
+      salt TEXT,
+      isAdmin BOOLEAN)`
+db.run(sql)
+db.close((err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Close the database connection.');
+});
